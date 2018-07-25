bbcca5c6d8ca7b7e93c960e0af7b97e1e7d53e73
jacksweat <jacksweat93@outlook.com>
2018-07-25 23:42:16 +0800

    db2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_143913) do
+ActiveRecord::Schema.define(version: 2018_07_25_153841) do
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_07_25_143913) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
 end
